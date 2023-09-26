98e09d3474272dc101fd9f9f524030b3e708e549
Nico-Stern <nstern2002@gmail.com>
2023-09-26 11:12:38 +0200

    Kiste Heben

--- Assets/Scripts/ColorMec/Kiste.cs
+++ Assets/Scripts/ColorMec/Kiste.cs
@@ -1,39 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
+using UnityEngine.UIElements;
 
 public class Kiste : ColorLibary
 {
 
     private CharacterColor CC;
     private Rigidbody2D rb;
-    
+
+    [SerializeField] private float Timer;
+    [SerializeField] float StartTimer;
+
+    [SerializeField] private bool isTouching;
 
     public override void Start()
     {
         base.Start();
         CC = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterColor>();
-        rb = GetComponent<Rigidbody2D>();
+        Timer = StartTimer;
     }
 
     private void Update()
     {
-        if ((rb.velocity.x < rb.velocity.y&& rb.velocity.y >0)||(rb.velocity.x > rb.velocity.y&& rb.velocity.y <0))
+        if (CC.farbZahl == farbZahl&&isTouching&&Input.GetKey(KeyCode.Space)&&Timer<=0)
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-        else if((rb.velocity.x > rb.velocity.y&& rb.velocity.x>0)||(rb.velocity.x > rb.velocity.y&& rb.velocity.x<0))
-        {
-            rb.velocity = new Vector2( rb.velocity.x,0);
+            this.transform.SetParent(GameObject.FindWithTag("Player").transform);
+            transform.localPosition = new Vector2(0,0.18f);
+            Timer = StartTimer;
         }
         
-        if (CC.farbZahl == farbZahl)
+        Timer -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)&&Timer<=0)
         {
-            rb.isKinematic = false;
+            isTouching = false;
+            transform.SetParent(null);
+            Timer = StartTimer;
         }
-        else
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
         {
-            rb.isKinematic = true;
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            isTouching = false;
         }
     }
 }
